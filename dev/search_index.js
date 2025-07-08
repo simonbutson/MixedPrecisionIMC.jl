5c904b89e5cd2e52438600dd07b928e54882efaf
Documenter.jl <documenter@juliadocs.github.io>
2025-07-08 20:24:35 +0000

    build based on 21e6409

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MixedPrecisionIMC","category":"page"},{"location":"#MixedPrecisionIMC","page":"Home","title":"MixedPrecisionIMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MixedPrecisionIMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MixedPrecisionIMC]","category":"page"}]
+[{"location":"#MixedPrecisionIMC","page":"Home","title":"MixedPrecisionIMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MixedPrecisionIMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
